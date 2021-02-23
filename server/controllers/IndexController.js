e05b5956f1727aec4372b765cdd80f73bd35d216
andri2621 <andri.setiawan996@gmail.com>
2021-02-23 11:19:58 +0700

    menambahkan index controller

--- server/controllers/IndexController.js
+++ server/controllers/IndexController.js
@@ -1,5 +1,22 @@
 import regions from "./RegionController"
+import city from "./city.controller"
+import account from "./account.controller"
+import address from "./address.controller"
+import kecamatan from "./kecamatan.controller"
+import kodepos from "./kodepos.controller"
+import province from "./province.controller"
+import users from "./users.controller"
+
+
 
 export default {
-    regions
+    regions,
+    city,
+    account,
+    address,
+    kecamatan,
+    kodepos,
+    province,
+    users
+    
 }
