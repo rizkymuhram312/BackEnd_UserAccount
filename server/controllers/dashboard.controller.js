1877e77608536761c5b56b34e9ebc94a18b259fb
rizkymuhram312 <kikiberem@gmail.com>
2021-03-29 13:58:43 +0700

    fix dashboard admin

--- server/controllers/dashboard.controller.js
+++ server/controllers/dashboard.controller.js
@@ -1,5 +1,57 @@
 import { sequelize, Op } from "../models/IndexModel";
 
+const TotalUser = async (req, res) => {
+  try {
+    const Tuser = await sequelize.query(
+      "select count(user_id) as total_user from users",
+      { type: sequelize.QueryTypes.SELECT }
+    );
+
+    return res.send(Tuser);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const TotalBrand = async (req, res) => {
+  try {
+    const Tbrand = await sequelize.query(
+      "select count(brand_name) as total_brand from brand",
+      { type: sequelize.QueryTypes.SELECT }
+    );
+
+    return res.send(Tbrand);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const TotalProduct = async (req, res) => {
+  try {
+    const Tpro = await sequelize.query(
+      "select count(prod_name) as total_product from product",
+      { type: sequelize.QueryTypes.SELECT }
+    );
+
+    return res.send(Tpro);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const TotalOrder = async (req, res) => {
+  try {
+    const Torder = await sequelize.query(
+      "select count(order_name) as total_order from orders",
+      { type: sequelize.QueryTypes.SELECT }
+    );
+
+    return res.send(Torder);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const JumlahOrder = async (req, res) => {
   try {
     const order = await sequelize.query(
@@ -16,7 +68,20 @@ const JumlahOrder = async (req, res) => {
 const ProdukLaris = async (req, res) => {
   try {
     const laris = await sequelize.query(
-      " select prod_name, count(orit_prod_id) as total from orders_line_items join product on prod_id = orit_prod_id join orders on orit_order_name = order_name where order_stat_name = 'CLOSED' group by prod_name order by total desc",
+      " select prod_name, count(orit_prod_id) as total from orders_line_items join product on prod_id = orit_prod_id join orders on orit_order_name = order_name group by prod_name order by total desc",
+      { type: sequelize.QueryTypes.SELECT }
+    );
+
+    return res.send(laris);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ProdukLarisImg = async (req, res) => {
+  try {
+    const laris = await sequelize.query(
+      " select prod_name, count(orit_prod_id),prim_path as total from orders_line_items join product on prod_id = orit_prod_id join product_images on prod_id = prim_prod_id join orders on orit_order_name = order_name group by prod_name order by total desc",
       { type: sequelize.QueryTypes.SELECT }
     );
 
@@ -29,7 +94,7 @@ const ProdukLaris = async (req, res) => {
 const ProdukCancel = async (req, res) => {
   try {
     const ProdCancel = await sequelize.query(
-      " select prod_name,count(prod_name) as total from account join orders on acco_id = order_acco_id_seller join orders_line_items on orit_order_name = order_name join product on orit_prod_id = prod_id where order_stat_name = 'PAID' group by prod_name",
+      " select prod_name,count(prod_name) as total from account join orders on acco_id = order_acco_id_seller join orders_line_items on orit_order_name = order_name join product on orit_prod_id = prod_id where order_stat_name = 'CANCELLED' group by prod_name",
       { type: sequelize.QueryTypes.SELECT }
     );
 
@@ -42,7 +107,7 @@ const ProdukCancel = async (req, res) => {
 const ProdCityBuyer = async (req, res) => {
     try {
       const CityBuy = await sequelize.query(
-        " select city_id,city_name, (select sum(total_order) from (select city_id,order_acco_id,count(order_name) as total_order from orders join account on acco_id=order_acco_id join address on addr_accu_id = acco_id join kodepos on addr_kodepos = kodepos join kecamatan on kodepos_kec_id = kec_id join city on kec_city_id = city_id where addr_is_primary = true group by order_acco_id,city_id)t where city_id=c.city_id group by t.city_id) as total_order from city c",
+        " select city_id,city_name, (select sum(total_order) from (select city_id,order_acco_id,count(order_name) as total_order from orders join account on acco_id=order_acco_id join address on addr_accu_id = acco_id join kodepos on addr_kodepos = kodepos join kecamatan on kodepos_kec_id = kec_id join city on kec_city_id = city_id where addr_is_primary = true and order_stat_name = 'CLOSED' group by order_acco_id,city_id)t where city_id=c.city_id group by t.city_id) as total_order from city c",
         { type: sequelize.QueryTypes.SELECT }
       );
   
@@ -56,7 +121,7 @@ const ProdCityBuyer = async (req, res) => {
   const ProdCitySeller = async (req, res) => {
     try {
       const CitySell = await sequelize.query(
-        "select city_id,city_name, (select sum(total_order) from (select city_id,order_acco_id_seller,count(order_name) as total_order from orders join account on acco_id=order_acco_id_seller join address on addr_accu_id = acco_id join kodepos on addr_kodepos = kodepos join kecamatan on kodepos_kec_id = kec_id join city on kec_city_id = city_id where addr_is_primary = true group by order_acco_id_seller,city_id)t where city_id=c.city_id group by t.city_id) as total_order from city c",
+        "select city_id,city_name, (select sum(total_order) from (select city_id,order_acco_id_seller,count(order_name) as total_order from orders join account on acco_id=order_acco_id_seller join address on addr_accu_id = acco_id join kodepos on addr_kodepos = kodepos join kecamatan on kodepos_kec_id = kec_id join city on kec_city_id = city_id where addr_is_primary = true and order_stat_name= 'CLOSED' group by order_acco_id_seller,city_id)t where city_id=c.city_id group by t.city_id) as total_order from city c",
         { type: sequelize.QueryTypes.SELECT }
       );
   
@@ -118,6 +183,18 @@ const ProdCityBuyer = async (req, res) => {
     }
   };
 
+  const ProductCS = async (req, res) => {
+    try {
+      const ProductSeller = await sequelize.query(
+        "select get_city_name(acco_id),prod_name,total_qty from(select acco_id,prod_name as prod_name,sum(orit_qty) as total_qty from orders_line_items join product on prod_id = orit_prod_id join orders on order_name = orit_order_name join account on acco_id = order_acco_id where order_stat_name = 'CLOSED' group by acco_id,prod_name)t",
+        { type: sequelize.QueryTypes.SELECT }
+      );
+  
+      return res.send(ProductSeller);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
 export default {
@@ -129,5 +206,12 @@ export default {
   TotalOrderCB,
   TotalOrderCS,
   TotalProductCB,
-  TotalProductCS
+  TotalProductCS,
+  ProdukLarisImg,
+  ProductCS,
+  TotalBrand,
+  TotalOrder,
+  TotalUser,
+  TotalProduct
+
 };
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -14,6 +14,11 @@ router.get('/OrderCBT',IndexController.dashboard.TotalOrderCB);
 router.get('/OrderCST',IndexController.dashboard.TotalOrderCS);
 router.get('/ProdCBT',IndexController.dashboard.TotalProductCB);
 router.get('/ProdCST',IndexController.dashboard.TotalProductCS);
+router.get('/Tuser',IndexController.dashboard.TotalUser);
+router.get('/Tbrand',IndexController.dashboard.TotalBrand);
+router.get('/Tproduct',IndexController.dashboard.TotalProduct);
+router.get('/Torder',IndexController.dashboard.TotalOrder);
+
 
 
 export default router;
